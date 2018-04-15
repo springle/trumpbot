4c23c07af4e481c68315c114004e4b6698002724
Angelina Wang <angelina.wang@berkeley.edu>
2018-04-15 12:23:24 -0700

    working speech

--- make_matrix.py
+++ make_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 
 """ Takes in the text file, and cleans it by dealing with punctuation and returns an array of
 all the tokens in the order they are said during the speech.
@@ -18,7 +19,6 @@ def return_token_array(text_file):
 	return tokens
 
 def make_transition_matrix(tokens):
-	tokens = tokens[:20]
 	ordered_tokens = np.unique(tokens)
 	n = len(ordered_tokens)
 	mat = np.zeros((n, n))
@@ -27,14 +27,42 @@ def make_transition_matrix(tokens):
 		mat[a][b] += 1
 	normalize = np.sum(mat, axis=1)
 	mat = mat / normalize[:, np.newaxis]
-	return mat
+	np.save('ordered_tokens', ordered_tokens)
+	np.save('transition_matrix', mat)
+	return mat, ordered_tokens
 
-def generate_random(mat):
-	return None
+def generate_random(mat, ordered_tokens, num_sentences):
+	curr_sentence = 0
+	speech_tokens = []
+	last_token = np.where(ordered_tokens == '.')[0][0]
+	print("last token: {}".format(last_token))
+	while curr_sentence < num_sentences:
+		row = mat[last_token]
+		indices = np.where(row != 0)[0]
+		values = [row[index] for index in indices]
+		picked = np.random.choice(indices, p=values)
+		last_token = picked
+		speech_tokens.append(ordered_tokens[picked])
+		if ordered_tokens[picked] == '.':
+			curr_sentence += 1
+	speech = ''
+	for tok in speech_tokens:
+		if tok not in [',', '.']:
+			speech += ' '
+		speech += tok
+	speech = speech[1:]
+
+	return speech
 
 if __name__ == '__main__':
 	tokens = return_token_array('speeches.txt')
-	make_transition_matrix(tokens)
+	if Path('transition_matrix.npy').exists() and Path('ordered_tokens.npy').exists():
+		mat, ordered_tokens = np.load('transition_matrix.npy'), np.load('ordered_tokens.npy')
+	else:
+		mat, ordered_tokens = make_transition_matrix(tokens)
+	# mat, ordered_tokens = make_transition_matrix(tokens)
+	speech = generate_random(mat, ordered_tokens, 5)
+	print(speech)
 
 
 
