67bd251371b09484e0292d8e2cdd1d04770f12c8
Sam Pringle <15springle@gmail.com>
2018-04-15 13:46:45 -0700

    Add matrix pregeneration to app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from main import TrumpBot
 application = Flask(__name__)
 
+# Trigger matrix generation
+print("Pre-generating matrix")
+TrumpBot.generate_speech()
+
 @application.route("/")
 def hello():
     return TrumpBot.generate_speech()
